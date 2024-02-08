e3802fd95fc4e749e64727af7b92247a51603bfa
pieterbrandsen <pieterbrandsen2@gmail.com>
2024-02-08 22:20:53 +0000

    Readded exporter file

--- src/exporter.js
+++ src/exporter.js
@@ -1,212 +1,259 @@
-/* eslint-disable no-async-promise-executor */
-import fetch from "node-fetch";
-import _ from "lodash";
-
-import { ScreepsAPI } from "screeps-api";
-
 import minimist from "minimist";
-import winston from "winston";
 
-let Config;
+import fs from "fs";
+import { WebhookClient } from "discord.js";
+import { PasteClient, Publicity, ExpireDate } from "pastebin-api";
+import { Octokit } from "@octokit/core";
+import beautify from "json-beautify";
+
 const argv = minimist(process.argv.slice(2));
 
-const logger = winston.createLogger({
-  level: "debug",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    new winston.transports.File({
-      filename: "logs/logListener.log",
-      level: "debug",
-    }),
-  ],
-});
+const discordUsername = argv.discordUsername || "Screeps Performance Server";
+const avatarURL = argv.discordAvatarUrl || "https://user-images.githubusercontent.com/48334001/189509241-7f04fe66-a5bc-4791-ada7-4f948794ceb0.png";
+const githubOwner = argv.githubOwner || "The-International-Screeps-Bot";
+const githubRepo = argv.githubRepo || "The-International-Open-Source";
+export default class Exporter {
+  static discordMessage = null;
 
-const filter = {
-  controller: (o) => {
-    if (o && o.type) {
-      return o.type === "controller";
-    }
-    return false;
-  },
-  creeps: (o) => {
-    if (o && o.type) {
-      return o.type === "creep";
-    }
-    return false;
-  },
-  structures: (o) => {
-    if (o && o.type) {
-      return o.type === "spawn" || o.type === "extension";
-    }
-    return false;
-  },
-};
+  static githubCommit = null;
 
-const hostname = "127.0.0.1";
+  static webhookClient = null;
 
-export default class Helper {
-  static async setConfig(config) {
-    Config = config;
-  }
+  static octokit = null;
 
-  /**
-   * followLog method
-   *
-   * Connects to the api and reads and prints the console log, if messages
-   * are available
-   *
-   * @param {list} rooms - The rooms
-   * @param {function} statusUpdater - Function to handle status updates
-   * @return {undefined}
-   */
-  static async followLog(rooms, statusUpdater) {
-    rooms.forEach(async (room) => {
-      const api = new ScreepsAPI({
-        email: room,
-        password: "password",
-        protocol: "http",
-        hostname,
-        port: Config.serverPort,
-        path: "/",
-      });
+  static commit = null;
 
-      await api.auth();
+  static commitName = "localhost";
 
-      api.socket.connect();
-      api.socket.on("connected", () => {});
-      api.socket.on("auth", () => {});
-      api.socket.subscribe(`room:${room}`, statusUpdater);
-      api.socket.subscribe("console", (event) => {
-        if (event.data.messages) {
-          event.data.messages.log
-            .filter((msg) => msg.includes("<p style='color:#bb3d3d;'>"))
-            .forEach((msg) => {
-              logger.debug(msg);
-            });
-        }
-      });
-    });
-  }
-  /**
-   * Spawn bot
-   * @param {string} botName
-   * @param {string} roomName
-   */
+  static customConstructor() {
+    if (process.env.GITHUB_EVENT_PATH) {
+      const file = fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8");
+      this.githubCommit = JSON.parse(file);
 
-  static async spawnBot(botName, roomName, roomsSeen, cliPort) {
-    console.log(`Spawn ${botName} in ${roomName}`);
-    await this.executeCliCommand(
-      `bots.spawn('${botName}', '${roomName}', {username: '${roomName}', auto:'true'})\r\n`,
-      cliPort
-    );
-    await this.setPassword(roomName, roomsSeen, Config.playerRooms, cliPort);
-  }
-
-  /**
-   * sets password for user
-   *
-   * @param {string} line
-   * @param {object} socket
-   * @param {list} rooms
-   * @param {object} roomsSeen
-   * @param {stringMap} playerRooms
-   * @return {boolean}
-   */
-  static async setPassword(roomName, roomsSeen, playerRooms, cliPort) {
-    // eslint-disable-next-line no-param-reassign
-    roomsSeen[roomName] = true;
-    console.log(`Set password for ${roomName}`);
-    // Password is 'password'
-    /* eslint max-len: ["error", 1300] */
-    await this.executeCliCommand(
-      `storage.db.users.update({username: '${roomName}'}, {$set: {password: 'd0347d74b308e046b399e151c3674297ddd1aba6d6e380c94ea8ec070393d17297a3407e9c17d3d4a308043e3fd219faecc9d0d4c548a6eab87549ec83fd0688197d14b84fa810935f694c14eadd6eac3b36e19405190b1e216b5c3b0b79f03815670ba8c0eb2e23d00f556b8fdfc35eaa6d3f8f734132196c70c921f29160b1f1a0ac1fe4c196c15aa7c2a5d8358ed89fff3ad4ddbe45f7fc5ecb1b4538940f31188a9a65af59b8481f6aa00fecebf4f8e7a91be877ec8610350a06bac16d666f255a73768a96cd1797c25c68aded637f96c7b0e9ad8e9f85997bced58c288f8df06f78b096750fadc128a345c01b76ab4f0feff6f5b89712ddfe6d9b7a713b05add43bd0c4b1c59b4a72d5b81a42570c0b1f7980a969913ba31baf88ef1213e46cb09577e249688e1d10be958e7c5dae4033a5cc174261b837b29134ea090df426ad9a3624fa2be2dbfd47c6a56d7cda99c30d74c05102b1ee05e09eba4cf3f785d40c94f22b24c4e47409f5ba123b98fa30d23498e07ee26d542487b3be480f7b51f23712aef06630d1ea1a057e44e0bb8fcc1709e457544051730140852e7b493b7d3cd23202405f3d81d605be47c792681ce2d548388feddad94f790d58fb887d89358c4c0b8a6d0148e01f7f2cfd613ac371d3e3bdc606189eafba726df2959c2ac6b4780068713cb79a687e65298a4aeee75a3ef47aab3a9b853407be', salt: '8592666ec92a801874b463ea4c0a0da519936246d54bc4c40391f9ac7c5a8000'}})`,
-      cliPort
-    );
-
-    if (playerRooms[roomName]) {
-      console.log(`Set steam id for ${roomName}`);
-      await this.executeCliCommand(
-        `storage.db.users.update({username: '${roomName}'}, {$set: {steam: {id: '${playerRooms[roomName]}'}}})`,
-        cliPort
-      );
-    }
-  }
-
-  /**
-   * sleep method
-   *
-   * Helper method to sleep for amount of seconds.
-   * @param {number} seconds Amount of seconds to sleep
-   * @return {object}
-   */
-  static sleep(seconds) {
-    // eslint-disable-next-line no-promise-executor-return
-    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
-  }
-
-  static initControllerID(event, status, controllerRooms) {
-    if (status[event.id].controller === null) {
-      // eslint-disable-next-line prefer-destructuring
-      status[event.id].controller = _.filter(
-        event.data.objects,
-        filter.controller
-      )[0];
-      // eslint-disable-next-line no-underscore-dangle
-      status[event.id].controller = status[event.id].controller._id;
-      controllerRooms[status[event.id].controller] = event.id;
-    }
-  }
-
-  static updateCreeps(event, status) {
-    const creeps = _.filter(event.data.objects, filter.creeps);
-    if (_.size(creeps) > 0) {
-      status[event.id].creeps += _.size(creeps);
-    }
-  }
-
-  static updateStructures(event, status) {
-    const structures = _.filter(event.data.objects, filter.structures);
-    if (_.size(structures) > 0) {
-      status[event.id].structures += _.size(structures);
-    }
-  }
-
-  static updateController(event, status, controllerRooms) {
-    const controllers = _.pick(
-      event.data.objects,
-      Object.keys(controllerRooms)
-    );
-    Object.keys(controllers).forEach((controllerId) => {
-      const controller = controllers[controllerId];
-      const roomName = controllerRooms[controllerId];
-      if (status[roomName] === undefined) {
-        return;
+      const object = this.githubCommit;
+      if (object.commits) {
+        this.commitName = object.commits[object.commits.length - 1].message;
       }
-      if (controller.progress >= 0) {
-        status[roomName].progress = controller.progress;
-      }
-      if (controller.level >= 0) {
-        status[roomName].level = controller.level;
-      }
-    });
+    }
   }
 
-  static async executeCliCommand(command, cliPort) {
+  static async sendDiscordWebHook(content, isPeriodic = true) {
+    if (!argv.discordWebHookUrl) return;
+
+    if (!this.webhookClient)
+      this.webhookClient = new WebhookClient({ url: argv.discordWebHookUrl });
+
     try {
-      const result = await fetch(`http://${hostname}:${cliPort}/cli`, {
-        method: "POST",
-        body: command,
-        headers: { "Content-Type": "text/plain" },
-      });
-      const text = await result.text();
-      console.log(`> ${command}`);
-      if (argv.debug) console.log(text);
-      await this.sleep(1);
-      return text;
+      const data = {
+        username: discordUsername + (isPeriodic ? " - Periodic " : " - Final"),
+        avatarURL,
+        content,
+      };
+
+      if (isPeriodic) {
+        if (!this.discordMessage)
+          this.discordMessage = await this.webhookClient.send(data);
+        else {
+          data.content = `${this.discordMessage.content}\n${content}`;
+          this.discordMessage.content = data.content;
+          await this.webhookClient.editMessage(this.discordMessage.id, data);
+        }
+      } else {
+        await this.webhookClient.send(data);
+      }
     } catch (error) {
-      console.log(error);
-      return undefined;
+      console.log("Failed to send discord webhook", error);
     }
+  }
+
+  static async sendGithubComment(content, isPeriodic = true) {
+    if (
+      !argv.githubAuth ||
+      !this.githubCommit ||
+      !argv.githubOwner ||
+      !argv.githubRepo
+    ) {
+      return;
+    }
+
+    if (!this.octokit) {
+      this.octokit = new Octokit({
+        auth: argv.githubAuth,
+      });
+    }
+
+    const baseComment = "**BOT COMMENT!**\n\n";
+    const url =
+      isPeriodic && this.githubCommit
+        ? "PATCH /repos/{owner}/{repo}/issues/comments/{comment_id}"
+        : "POST /repos/{owner}/{repo}/issues/{issue_number}/comments";
+    let body = "";
+    if (!isPeriodic) {
+      body = baseComment + content;
+    } else if (this.githubCommit) {
+      body = `${this.githubCommit.data.body}\n${content}`;
+    } else {
+      body = baseComment + content;
+    }
+
+    try {
+      const response = await this.octokit.request(url, {
+        owner: githubOwner,
+        repo: githubRepo,
+        issue_number: this.githubCommit.number,
+        body,
+        comment_id: this.githubCommit ? this.githubCommit.data.id : null,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      });
+
+      if (isPeriodic && !this.githubCommit) this.githubCommit = response;
+    } catch (error) {
+      console.log("Failed to send github comment", error);
+    }
+  }
+
+  static getLoggerFile() {
+    if (fs.existsSync("./logs/logListener.log", "utf8")) {
+      const fileText = fs.readFileSync("./logs/logListener.log", "utf8");
+      const fileLineCount = fileText.split("\n").length;
+      return { success: true, text: fileText, lineCount: fileLineCount };
+    }
+    return { success: false, text: "No log dump file found" };
+  }
+
+  static getHistory() {
+    if (fs.existsSync("./logs/history.log", "utf8")) {
+      const fileText = fs.readFileSync("./logs/history.log", "utf8");
+      const fileLineCount = fileText.split("\n").length;
+      return { success: true, text: fileText, lineCount: fileLineCount };
+    }
+    return { success: false, text: "No history file found" };
+  }
+
+  static async createPasteBinUrl(content) {
+    const key = argv.pasteBinUrlDevKey;
+    if (!key) return "No key provided";
+
+    const client = new PasteClient(key);
+    try {
+      const url = await client.createPaste({
+        code: content,
+        expireDate: ExpireDate.OneYear,
+        name: this.commitName,
+        publicity: Publicity.Unlisted,
+      });
+      return `<${url}>`;
+    } catch (error) {
+      if (error.includes("maximum pastes")) {
+        return "Rate limit reached";
+      }
+      return "ERROR";
+    }
+  }
+
+  static generatePeriodicMessage(gameTime, milestones, startTime) {
+    let message = "";
+    if (!this.discordMessage) {
+      message = `**Performance Test Results**\n**Commit:** ${this.commitName}\n`;
+    }
+
+    const endTime = new Date();
+    const timeRun = (endTime - startTime) / 1000 / 60 / 60; // in hours
+
+    message +=
+      `\n**Game Time:** ${gameTime}\n` +
+      `**Result:** ${milestones.success ? "Success :) " : "Fail :( "}\n` +
+      `**Time Run:** ${timeRun.toFixed(2)} hours\n\n` +
+      `**Milestone:** \n\`\`\`json\n${beautify(milestones, null, 2, 80)}\`\`\``;
+
+    return message;
+  }
+
+  static generateFinalMessage(
+    milestones,
+    fails,
+    status,
+    lastTickNumber,
+    startTime,
+    loggerFile,
+    historyFile
+  ) {
+    const endTime = new Date();
+    const timeRun = (endTime - startTime) / 1000 / 60 / 60; // in hours
+
+    const loggerText = loggerFile.success
+      ? `**Filtered Logs:** ${loggerFile.pasteBinUrl} (${loggerFile.lineCount - 1
+      } logs)\n`
+      : "**Filtered Logs:** No log dump file found\n";
+    const historyText = historyFile.success
+      ? `**Milestone History:** ${historyFile.pasteBinUrl} (${historyFile.lineCount - 1
+      } logs)\n`
+      : "**Milestone History:** No history file found\n";
+
+    return (
+      `**Performance Test Results**\n` +
+      `**Commit:** ${this.commitName}\n` +
+      `**Fails:** ${fails.length === 0 ? "No fails!" : `${fails.length} fails`
+      }\n` +
+      `**Game Time :** ${lastTickNumber}\n` +
+      `**Time:** Start: ${new Date(startTime).toISOString()} - End: ${new Date(
+        endTime
+      ).toISOString()}\n` +
+      `**Time Run:** ${timeRun.toFixed(2)} hours\n\n` +
+      `**Milestones:** \n\n\`\`\`json\n${milestones.reduce((a, b) =>
+        typeof a === "string"
+          ? a
+          : `${beautify(a, null, 2, 80) + beautify(b, null, 2, 80)}, `
+      )}\`\`\`\n` +
+      `**Status:** \n\`\`\`json\n${beautify(
+        status,
+        null,
+        2,
+        80
+      )}\`\`\`\n\n${loggerText}${historyText}`
+    );
+  }
+
+  static async sendPeriodicResult(gameTime, milestone, startTime) {
+    const message = this.generatePeriodicMessage(
+      gameTime,
+      milestone,
+      startTime
+    );
+    await this.sendDiscordWebHook(message);
+    await this.sendGithubComment(message);
+  }
+
+  static async sendFinalResult(
+    milestones,
+    fails,
+    status,
+    lastTickNumber,
+    startTime
+  ) {
+    const logs = this.getLoggerFile();
+    const history = this.getHistory();
+
+    const message = this.generateFinalMessage(
+      milestones,
+      fails,
+      status,
+      lastTickNumber,
+      startTime,
+      {
+        pasteBinUrl: await this.createPasteBinUrl(logs.text),
+        lineCount: logs.lineCount,
+        success: logs.success,
+      },
+      {
+        pasteBinUrl: await this.createPasteBinUrl(history.text),
+        lineCount: history.lineCount,
+        success: history.success,
+      }
+    );
+    await this.sendDiscordWebHook(message, false);
+    await this.sendGithubComment(message, false);
   }
 }
